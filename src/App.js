f5a337338a7e5a868b8cb653381681529429c0f9
Eric4K <ejhuang@umass.edu>
2022-11-12 11:26:51 -0500

    add login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavigationBar from './components/NavigationBar';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"; // activate and direct to different link
 import './App.css';
 import Home from "./components/pages/home"
+import { LogIn } from './components/pages/LogIn';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Home/>
       <Routes>
         <Route path = '/' exact component={Home} />
+        <Route path = 'login' element={<LogIn />} />
       </Routes>
     </Router>
     </>

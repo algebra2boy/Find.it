d9c29ca71062d3f8e0f2ba390267fa106561627b
noonbles <atrac480@gmail.com>
2022-11-12 21:21:55 -0500

    very minor change to path

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,21 +13,21 @@ function Cards() {
                       src = "images/image-1.png"
                       text = "This website is aimed to help people to search for their lost items, and for people who find lost items to help those items find their owners."
                       label = "Mission"
-                      path = "/services"
+                      path = "/"
                     />
 
                     <CardItem 
                       src = "images/hackumass.png"
-                      text = " " //WHAT DO I ADD HERE?!?!?!
+                      text = "This is our submission to the UMASS Hackathon 2022!"
                       label = "Hackathon"
-                      path = "/services"
+                      path = "/"
                     />
 
                     <CardItem 
                       src = "images/lost_and_found.png"
-                      text = "1. Make an account 2. Login 3. Search for your item, or post a lost one!"
+                      text = "1. Make an account 2. Login \n 3. Search for your item, or post a lost one!"
                       label = "Steps"
-                      path = "/services"
+                      path = "/"
                     />
 
                     
@@ -38,14 +38,14 @@ function Cards() {
                       src = "images/image-1.png" //IF WE DONT ALREADY, CREATE A PATH TO THE FILE (AND MAKE IT IF WE HAVE TO) THEN GIVE THE PATH
                       text = "Most recent lost item" 
                       label = "item1" //GET THE NAME OF THE ITEM
-                      path = "/services"
+                      path = "/"
                     />
 
                     <CardItem //SAME AS ABOVE ONLY WE TAKE THE OLDEST DATA
                       src = "images/image-1.png"
                       text = "Oldest lost item"
                       label = "item2"
-                      path = "/services"
+                      path = "/"
                     />
                 </ul>
             </div>
